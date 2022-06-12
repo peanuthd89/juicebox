f0d3015db71deeb60bcf986230beb17cea0cba34
peanuthd89 <100543185+peanuthd89@users.noreply.github.com>
2022-06-12 11:22:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,54 @@ const server = express();
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
+require('dotenv').config();
+
+// remove this once you confirm it works
+console.log(process.env.JWT_SECRET);
+// like, seriously. go delete that!
+
+// EVERYTHING ELSE
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+
+server.use(morgan('dev'));
+server.use(express.json())
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+server.use(async (req, res, next) => {
+    const prefix = 'Bearer '
+    const auth = req.headers['Authorization'];
+  
+    if (!auth) {
+      next(); // don't set req.user, no token was passed in
+    }
+  
+  
+    if (auth.startsWith(prefix)) {
+      // recover the token
+      const token = auth.slice(prefix.length);
+      try {
+        // recover the data
+        const { id } = jwt.verify(data, 'secret message');
+  
+        // get the user from the database
+        const user = await getUserById(id);
+        // note: this might be a user or it might be null depending on if it exists
+  
+        // attach the user and move on
+        req.user = user;
+  
+        next();
+      } catch (error) {
+        // there are a few types of errors here
+      }
+    }
+  })
